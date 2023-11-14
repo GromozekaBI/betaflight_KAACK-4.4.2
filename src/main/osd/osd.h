c97705cbd78f74b16a05feb864007b50f90eb8ce
GromozekaBI <150791799+GromozekaBI@users.noreply.github.com>
2023-11-14 10:44:20 +0000

    OSD_RCCHANNELS_COUNT

--- src/main/osd/osd.h
+++ src/main/osd/osd.h
@@ -42,7 +42,7 @@ extern const char * const osdTimerSourceNames[OSD_NUM_TIMER_TYPES];
 #define OSD_PROFILE_COUNT 1
 #endif
 
-#define OSD_RCCHANNELS_COUNT 4
+#define OSD_RCCHANNELS_COUNT 12
 
 #define OSD_CAMERA_FRAME_MIN_WIDTH  2
 #define OSD_CAMERA_FRAME_MAX_WIDTH  30    // Characters per row supportes by MAX7456
