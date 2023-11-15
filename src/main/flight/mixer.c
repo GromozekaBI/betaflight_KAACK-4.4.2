afee93c9e996ba6feb376985b02d473920cf44c6
GromozekaBI <150791799+GromozekaBI@users.noreply.github.com>
2023-11-15 20:36:50 +0000

    dynamic debug

--- src/main/flight/mixer.c
+++ src/main/flight/mixer.c
@@ -378,8 +378,9 @@ static void applyRpmLimiter(mixerRuntime_t *mixer)
     DEBUG_SET(DEBUG_RPM_LIMIT, 2, lrintf(mixer->rpmLimiterThrottleScale * 100.0f)); // текущий расчетный предел дросельной заслонки
     //DEBUG_SET(DEBUG_RPM_LIMIT, 3, lrintf(throttle * 100.0f)); //текущее значение газа
     int dynamic = rcCommand[AUX5];
-    dynamic = constrainf(dynamic / (PWM_RANGE_MAX-PWM_RANGE_MIN), 0.0f, 1.0f);
-    DEBUG_SET(DEBUG_RPM_LIMIT, 3, lrintf(dynamic * 100.0f)); //текущее 5 AUX канала
+    //dynamic = constrainf(dynamic / (PWM_RANGE_MAX-PWM_RANGE_MIN), 0.0f, 1.0f);
+    dynamic = dynamic / 100.0f;
+    DEBUG_SET(DEBUG_RPM_LIMIT, 3, lrintf(dynamic * 1.0f)); //текущее 5 AUX канала
 }
 #endif // USE_RPM_LIMIT
 
