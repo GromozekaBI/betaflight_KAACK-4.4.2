5891fb63c3cc386522c2a46bc7c05fa652159460
unknown <boyarinov.i.r@yandex.ru>
2024-03-29 13:14:28 +0300

    more data v1.2.0

--- src/main/flight/imu.c
+++ src/main/flight/imu.c
@@ -590,10 +590,10 @@ void imuUpdateAttitude(timeUs_t currentTimeUs)
     //DEBUG_SET(DEBUG_BIG_BLACK, 0, lrintf(attitude.raw[FD_PITCH] - accelerometerConfig()->accelerometerTrims.raw[FD_PITCH])); // РАБОТАЕТ!!!
     //DEBUG_SET(DEBUG_BIG_BLACK, 3, lrintf(attitude.raw[FD_ROLL] - accelerometerConfig()->accelerometerTrims.raw[FD_ROLL])); // РАБОТАЕТ!!!
     //DEBUG_SET(DEBUG_BIG_BLACK, 3, lrintf(attitude.raw[FD_YAW] - accelerometerConfig()->accelerometerTrims.raw[FD_YAW])); 
-    DEBUG_SET(DEBUG_BIG_BLACK, 0, lrintf(attitude.raw[FD_PITCH] - accelerometerConfig()->accelerometerTrims.raw[FD_PITCH])); // РАБОТАЕТ!!!
-    DEBUG_SET(DEBUG_BIG_BLACK, 1, lrintf(attitude.raw[FD_PITCH])); // РАБОТАЕТ!!!
-    DEBUG_SET(DEBUG_BIG_BLACK, 2, lrintf(attitude.raw[FD_ROLL])); // РАБОТАЕТ!!!
-    DEBUG_SET(DEBUG_BIG_BLACK, 3, lrintf(attitude.raw[FD_YAW])); // РАБОТАЕТ!!!
+    //DEBUG_SET(DEBUG_BIG_BLACK, 0, lrintf(attitude.raw[FD_PITCH] - accelerometerConfig()->accelerometerTrims.raw[FD_PITCH])); // РАБОТАЕТ!!!
+    DEBUG_SET(DEBUG_BIG_BLACK, 10, lrintf(attitude.raw[FD_PITCH])); // РАБОТАЕТ!!!
+    DEBUG_SET(DEBUG_BIG_BLACK, 11, lrintf(attitude.raw[FD_ROLL])); // РАБОТАЕТ!!!
+    DEBUG_SET(DEBUG_BIG_BLACK, 12, lrintf(attitude.raw[FD_YAW])); // РАБОТАЕТ!!!
 }
 #endif // USE_ACC
 
